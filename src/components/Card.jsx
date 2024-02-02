febf8161aa70433cf00d2b0d72f389441a962fb4
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-02-02 17:51:12 +0530

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Card.css"
+
+const Card = (props) => {
+  return (
+    <div>
+      <div className="card" style={{overflow: "hidden"}}>
+        <img src="https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages.png?v=1619126283" alt="" width = {247} 
+        style={{border: "2px solid black"}} />
+        <h1>{props.title}</h1>
+        <p>{props.desc}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Copyright &copy; codewithabhra.com | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
